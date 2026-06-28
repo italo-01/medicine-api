7ffa0c48219a9ec94264e04b1098baaad59fb2a0
ITALO COSTA RIBEIRO <italocosta019641@gmail.com>
2026-06-28 15:03:04 -0300

    fix: corrige retorno dos endpoints do CRUD para retornar dados completos

--- src/main/java/com/udemy/PrimeiroProjetoSpring/controller/RemedioController.java
+++ src/main/java/com/udemy/PrimeiroProjetoSpring/controller/RemedioController.java
@@ -3,9 +3,10 @@ package com.udemy.PrimeiroProjetoSpring.controller;
 import com.udemy.PrimeiroProjetoSpring.remedio.*;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -18,39 +19,54 @@ public class RemedioController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid DadosCadastroRemedio dados) {
-        repository.save(new Remedio(dados));
+    public ResponseEntity<DadosDetalhamentoRemedio> cadastrar(@RequestBody @Valid DadosCadastroRemedio dados, UriComponentsBuilder uribuilder) {
+        var remedio = new Remedio(dados);
+        repository.save(remedio);
 
+        var uri = uribuilder.path("/remedios/{id}").buildAndExpand(remedio.getId()).toUri();
+
+        return  ResponseEntity.created(uri).body(new DadosDetalhamentoRemedio(remedio));
     }
 
     @GetMapping
-    public List<DadosListagemRemedios> listar() {
-        return repository.findAllByAtivoTrue().stream().map(DadosListagemRemedios::new).toList();
+    public ResponseEntity<List<DadosListagemRemedios>> listar() {
+        var lista = repository.findAllByAtivoTrue().stream().map(DadosListagemRemedios::new).toList();
+
+        return ResponseEntity.ok(lista);
     }
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizarRemedio dados) {
+    public ResponseEntity<DadosDetalhamentoRemedio> atualizar(@RequestBody @Valid DadosAtualizarRemedio dados) {
         var remedio = repository.getReferenceById(dados.id());
         remedio.atualizarInformacoes(dados);
+
+
+        return ResponseEntity.ok(new DadosDetalhamentoRemedio(remedio));
     }
     @DeleteMapping("/{id}")
     @Transactional
-    public void deletar(@PathVariable long id){
-        repository.deleteById(id);
+    public ResponseEntity<Void>deletar(@PathVariable long id){
+          repository.deleteById(id);
+
+          return ResponseEntity.noContent().build();
     }
 
-    @DeleteMapping("inativar/{id}")
+    @DeleteMapping("/inativar/{id}")
     @Transactional
-    public void inativar (@PathVariable long id){
+    public ResponseEntity<Void> inativar (@PathVariable long id){
         var remedio = repository.getReferenceById(id);
         remedio.inativarRemedio();
+
+        return ResponseEntity.noContent().build();
     }
 
-    @PutMapping("ativar/{id}")
+    @PutMapping("/ativar/{id}")
     @Transactional
-    public void ativar(@PathVariable long id){
+    public ResponseEntity<Void> ativar(@PathVariable long id){
         var remedio = repository.getReferenceById(id);
         remedio.ativarRemedio();
+
+        return ResponseEntity.noContent().build();
     }
 }
---
+++ src/main/java/com/udemy/PrimeiroProjetoSpring/remedio/DadosDetalhamentoRemedio.java
@@ -0,0 +1,24 @@
+package com.udemy.PrimeiroProjetoSpring.remedio;
+
+import java.time.LocalDate;
+
+public record DadosDetalhamentoRemedio(
+                 Long id,
+                 String nome,
+                 Via via,
+                 String lote,
+                 int quantidade,
+                 LocalDate validade,
+                 Laboratorio laboratorio,
+                 Boolean ativo){
+    public DadosDetalhamentoRemedio(Remedio remedio){
+        this(remedio.getId(),
+             remedio.getNome(),
+             remedio.getVia(),
+             remedio.getLote(),
+             remedio.getQuantidade(),
+             remedio.getValidade(),
+             remedio.getLaboratorio(),
+             remedio.getAtivo());
+    }
+}
