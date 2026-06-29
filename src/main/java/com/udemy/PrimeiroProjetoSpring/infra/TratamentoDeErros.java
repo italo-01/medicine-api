990dd8c75018a5248b9be1bbdbc8b9305e45c3f4
ITALO COSTA RIBEIRO <italocosta019641@gmail.com>
2026-06-28 22:14:50 -0300

    feat: trata erro  para o  metodo POST
    Foi criado o metodo tratador 400 para retorna exatamente aonde foi identificado a exceção.

--- src/main/java/com/udemy/PrimeiroProjetoSpring/infra/TratamentoDeErros.java
+++ src/main/java/com/udemy/PrimeiroProjetoSpring/infra/TratamentoDeErros.java
@@ -2,6 +2,8 @@ package com.udemy.PrimeiroProjetoSpring.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,4 +14,19 @@ public class TratamentoDeErros {
     public ResponseEntity<?> tratador404(){
         return ResponseEntity.notFound().build();
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> tratador400 (MethodArgumentNotValidException exc){
+        var erros = exc.getFieldErrors();
+
+        return  ResponseEntity.badRequest().body(erros.stream().map(DadosErros::new).toList());
+    }
+    public record DadosErros(String campo, String mesagem){
+        public DadosErros (FieldError erro){
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+
 }
+
+
