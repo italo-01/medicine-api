c937ec17d0f2fcc2ba1820eeedb1cb58f871d308
ITALO COSTA RIBEIRO <italocosta019641@gmail.com>
2026-07-01 21:34:44 -0300

    Feat Cria entidade USER, e a tabela

---
+++ src/main/java/com/udemy/PrimeiroProjetoSpring/usuarios/Usuario.java
@@ -0,0 +1,20 @@
+package com.udemy.PrimeiroProjetoSpring.usuarios;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "Usuario")
+@Entity(name = "usuarios")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+}
