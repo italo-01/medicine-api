19a644de62c883db1ab1589a4259b2e008f1db07
ITALO COSTA RIBEIRO <italocosta019641@gmail.com>
2026-07-28 00:13:33 -0300

    Feat: chama a classe SecurityContext
    foi chamada a classe com objetivo de persitir a autenticação do usuario, para não da erro 403

--- src/main/java/com/udemy/PrimeiroProjetoSpring/infra/SecurityFilter.java
+++ src/main/java/com/udemy/PrimeiroProjetoSpring/infra/SecurityFilter.java
@@ -1,10 +1,14 @@
 package com.udemy.PrimeiroProjetoSpring.infra;
 
+import com.udemy.PrimeiroProjetoSpring.usuarios.Usuario;
+import com.udemy.PrimeiroProjetoSpring.usuarios.UsuarioRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,6 +19,9 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
     private TokenService tokenService;
 
+    @Autowired
+    private UsuarioRepository repository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
@@ -22,6 +29,11 @@ public class SecurityFilter extends OncePerRequestFilter {
 
         if (tokenJWt != null) {
             var subject = tokenService.getSubject(tokenJWt);
+            var usuario = repository.findByLogin(subject);
+
+            var authetication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authetication);
         }
 
         filterChain.doFilter(request, response);
