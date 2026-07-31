3551f2c3ea3edc4c15aa39cd9e2fa17b88a91df6
ITALO COSTA RIBEIRO <italocosta019641@gmail.com>
2026-07-31 19:33:40 -0300

    docs: Melhora documentação da API no Swagger

--- src/main/java/com/udemy/PrimeiroProjetoSpring/SwaggerConfiguration.java
+++ src/main/java/com/udemy/PrimeiroProjetoSpring/SwaggerConfiguration.java
@@ -1,6 +1,7 @@
 package com.udemy.PrimeiroProjetoSpring;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +11,11 @@ public class SwaggerConfiguration {
     @Bean
     public OpenAPI customOpenAPI(){
 
-        return new OpenAPI();
+        return new OpenAPI().info(new Info()
+                .title("REMEDIOS")
+                .version("1.0.0")
+                .description("Documento da Cafetaria API")
+        );
 
     }
 
--- src/main/java/com/udemy/PrimeiroProjetoSpring/controller/RemedioController.java
+++ src/main/java/com/udemy/PrimeiroProjetoSpring/controller/RemedioController.java
@@ -1,10 +1,13 @@
 package com.udemy.PrimeiroProjetoSpring.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import com.udemy.PrimeiroProjetoSpring.DTOs.DadosAtualizarRemedio;
 import com.udemy.PrimeiroProjetoSpring.DTOs.DadosCadastroRemedio;
 import com.udemy.PrimeiroProjetoSpring.DTOs.DadosDetalhamentoRemedio;
 import com.udemy.PrimeiroProjetoSpring.DTOs.DadosListagemRemedios;
 import com.udemy.PrimeiroProjetoSpring.remedio.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/remedio")
+@Tag(name = "Remedios ", description = "Geranciador de Remedios")
 public class RemedioController {
 
     @Autowired
@@ -25,6 +29,7 @@ public class RemedioController {
 
     @PostMapping
     @Transactional
+    @Operation(summary = "Cadastrar Remedios", description = "Cadastra um Novo remedio")
     public ResponseEntity<DadosDetalhamentoRemedio> cadastrar(@RequestBody @Valid DadosCadastroRemedio dados, UriComponentsBuilder uribuilder) {
         var remedio = new Remedio(dados);
         repository.save(remedio);
@@ -34,6 +39,7 @@ public class RemedioController {
         return  ResponseEntity.created(uri).body(new DadosDetalhamentoRemedio(remedio));
     }
 
+    @Operation(summary = "Listar Remedios", description = "Listar todos os remedios")
     @GetMapping
     public ResponseEntity<List<DadosListagemRemedios>> listar() {
         var lista = repository.findAllByAtivoTrue().stream().map(DadosListagemRemedios::new).toList();
@@ -43,6 +49,7 @@ public class RemedioController {
 
     @PutMapping
     @Transactional
+    @Operation(summary = "Atualizar Remedio", description = "Atualizar Remedio escolhido")
     public ResponseEntity<DadosDetalhamentoRemedio> atualizar(@RequestBody @Valid DadosAtualizarRemedio dados) {
         var remedio = repository.getReferenceById(dados.id());
         remedio.atualizarInformacoes(dados);
@@ -53,6 +60,7 @@ public class RemedioController {
 
     @DeleteMapping("/{id}")
     @Transactional
+    @Operation(summary = "Excluir Remedio", description = "Esclui Remedio escolhido")
     public ResponseEntity<Void>deletar(@PathVariable long id){
           repository.deleteById(id);
 
@@ -61,6 +69,7 @@ public class RemedioController {
 
     @DeleteMapping("/inativar/{id}")
     @Transactional
+    @Operation(summary = "Inativar Remedio", description = "Deixa o Remedio inativo")
     public ResponseEntity<Void> inativar (@PathVariable long id){
         var remedio = repository.getReferenceById(id);
         remedio.inativarRemedio();
@@ -70,6 +79,7 @@ public class RemedioController {
 
     @PutMapping("/ativar/{id}")
     @Transactional
+    @Operation(summary = "Ativar Remedio", description = "Deixa Remedio Ativo")
     public ResponseEntity<Void> ativar(@PathVariable long id){
         var remedio = repository.getReferenceById(id);
         remedio.ativarRemedio();
@@ -78,6 +88,7 @@ public class RemedioController {
     }
 
     @GetMapping("{id}")
+    @Operation(summary = "Buscar remedio", description = "Busca o remdio escolhido pelo ID")
     public ResponseEntity<DadosDetalhamentoRemedio> buscar(@PathVariable long id){
         var remedio = repository.getReferenceById(id);
 
